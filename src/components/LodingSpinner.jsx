3082340d8c4ff1be71e59ba8caaae093a5b9c176
Aftab Farhan <aftabfarhan324@gmail.com>
2025-11-09 00:20:59 +0600

    all folders stuchers complet

---
+++ src/components/LodingSpinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LodingSpinner = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LodingSpinner;
---
+++ src/pages/AddJod.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddJod = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddJod;
